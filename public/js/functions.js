eaa7514d4ee0706232423451e8b1362969e5d1e1
Felipe Lopes <34137405+fplxp3s@users.noreply.github.com>
2017-12-03 00:38:19 -0200

    Update functions.js

--- public/js/functions.js
+++ public/js/functions.js
@@ -199,7 +199,7 @@ function montaGraficoLojasMaisVenderam(dados) {
         totalFaturamento += parseFloat(value.valor_vendas);
     });
 
-    htmlTabela += '<tr><td colspan="2"></td><td>Faturamento Total: <span class="text-success"><strong>R$ '+parseFloat(totalFaturamento).toLocaleString()   +'</strong></span></td><td>Total: <span class="text-success"><strong>R$ '+parseFloat(totalComissao).toLocaleString()   +'</strong></span></td></tr>';
+    htmlTabela += '<tr><td colspan="2"></td><td>Faturamento Total: <span class="text-success"><strong>R$ '+parseFloat(totalFaturamento).toLocaleString()   +'</strong></span></td><td>Comissão Total: <span class="text-success"><strong>R$ '+parseFloat(totalComissao).toLocaleString()   +'</strong></span></td></tr>';
 
     $('#listaLojaMaisVendas').html(htmlTabela);
 
