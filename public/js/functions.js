fb69f24c4877d3f50e4ddc9d6d7fc239524f260a
Felipe Lopes <34137405+fplxp3s@users.noreply.github.com>
2017-12-03 04:32:37 -0200

    Update functions.js

--- public/js/functions.js
+++ public/js/functions.js
@@ -316,7 +316,7 @@ function montaGraficoProdutosMaisPesquisadosCloud(dados) {
 	
 
 	
-	var lines = text.split(/[,\. ]+/g),
+	var lines = text.split(/[,\.]+/g),
         data = Highcharts.reduce(lines, function (arr, word) {
             var obj = Highcharts.find(arr, function (obj) {
                 return obj.name === word;
