41ae5251bf89b66b6d2e86255bf0637cff6ee58d
Felipe Lopes <34137405+fplxp3s@users.noreply.github.com>
2017-12-04 15:28:08 -0200

    Update UsuarioService.php

--- app/Services/UsuarioService.php
+++ app/Services/UsuarioService.php
@@ -26,7 +26,7 @@ class UsuarioService extends Service
 
     public function listaCompras($id)
     {
-        return Compra::where('user_id', '=', $id)->paginate(10)->sortByDesc();
+        return Compra::where('user_id', '=', $id)->paginate(10)->sortByDesc('data');
     }
 
     public function buscaCompra($compraId)
